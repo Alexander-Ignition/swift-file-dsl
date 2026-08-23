50a21be817748759148699690d04d6bc8b434159
Alexander Ignition <izh.sever@gmail.com>
2026-08-23 19:27:53 +0300

    Update FileTests.swift
    test failure

--- Tests/FileDSLTests/FileTests.swift
+++ Tests/FileDSLTests/FileTests.swift
@@ -22,7 +22,7 @@ struct FileTests {
             └── 2.txt
             """
         #expect(fileHierarchy.treeDescription == tree)
-        #expect(fileHierarchy.fileTreeCommands.count == 3)
+        #expect(fileHierarchy.fileTreeCommands.count == 2)
         #expect(type(of: fileHierarchy) == _FileGroup<File, File, File>.self)
     }
 
